441591fa799c853cfcb9b8d4c2ae75d5ce35dbcf
andrea <atoaiari@gmail.com>
2020-12-15 11:42:50 +0100

    update flask skeleton

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    API_KEY = "bkb3u2pn4UTrNow1C14decjBV"
-    API_SECRET = "GDtds66AA927ahj1D5ovYCrN3CTFO1htzj9BQkiOHc9Sek5Er0"
+    API_KEY = ""
+    API_SECRET = ""
 
     ACCESS_TOKEN = ""
     ACCESS_TOKEN_SECRET = ""
